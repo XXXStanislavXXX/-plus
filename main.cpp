d52db76b393dd8db97cfda1bc0c40d7b2456bde5
Stanislav <stasperfiliyev@gmail.com>
2025-05-12 23:37:59 +0500

    8.6 Практическая работа

--- main.cpp
+++ main.cpp
@@ -3,54 +3,51 @@
 #include <corecrt_math_defines.h>
 
 /*
-Задача 4. Убийца Photoshop
-Вы разрабатываете новый крутой графический редактор. Один из инструментов
-редактора позволяет делать градиент — плавный переход между двумя точками 
-разной яркости.
-Яркость точки определяется целым числом от 0 до 255, где 0 — чёрный, 255 —
-белый, а всё, что между ними — оттенки серого.
-
-Напишите код для расчёта яркости точки градиента между двумя заданными точками.
-Пользователь вводит яркость левой границы градиента, потом яркость правой
-границы и параметр: число от 0 до 1, которое означает интересующую нас точку.
-Например, если параметр равен 0.5, то искомая точка находится ровно посередине
-между левой и правой границами, если параметр равен 0, то искомая точка
-совпадает с левой границей, и, наконец, если параметр равен 1, то искомая точка
-совпадает с правой границей. Программа должна вывести число — яркость искомой
-точки. Обеспечьте контроль ввода.
+Задание 6. Маятник
+Что нужно сделать
+Известно, что амплитуда качающегося маятника с каждым разом затухает на 8,4% от
+амплитуды прошлого колебания. Если качнуть маятник, он, строго говоря, никогда
+не остановится: его амплитуда будет уменьшаться до тех пор, пока мы не сочтём
+такой маятник остановившимся. Напишите программу, определяющую, сколько раз
+качнётся маятник, прежде чем он, по нашему мнению, остановится. Программа
+получает на вход начальную амплитуду колебания в сантиметрах и конечную
+амплитуду его колебаний, которая считается остановкой маятника. Обеспечьте
+контроль ввода.
 */
 
 int main()
 {
-    int leftBrightness, rightBrightness;
-    float parameter;
+    float startAmplitude, endAmplitude;
+    std::cout << "Enter start amplitude: ", std::cin >> startAmplitude;
 
-    std::cout << "Enter the brightness of the left boundary (0-255): ";
-    std::cin >> leftBrightness;
-
-    while (leftBrightness < 0 || leftBrightness > 255) {
-        std::cout << "Invalid brightness. Try again: ";
-        std::cin >> leftBrightness;
+    while(startAmplitude <= 0)
+    {
+        std::cout << "Start amplitude must be greater than 0. Try again: ", std::cin >> startAmplitude;
     }
 
-    std::cout << "Enter the brightness of the right boundary (0-255): ";
-    std::cin >> rightBrightness;
-    while (rightBrightness < 0 || rightBrightness > 255) {
-        std::cout << "Invalid brightness. Try again: ";
-        std::cin >> rightBrightness;
+    std::cout << "Enter end amplitude: ", std::cin >> endAmplitude;
+
+    while(endAmplitude <= 0)
+    {
+        std::cout << "End amplitude must be greater than 0. Try again: ", std::cin >> endAmplitude;
     }
 
-    std::cout << "Enter the parameter (0-1): ";
-    std::cin >> parameter;
+    if(startAmplitude < endAmplitude)
+    {
+        std::cout << "Start amplitude must be greater than end amplitude. Try again: ", std::cin >> startAmplitude, endAmplitude;
+    }
     
-    while (parameter < 0 || parameter > 1) {
-        std::cout << "Invalid parameter. Try again: ";
-        std::cin >> parameter;
+    int count = 0;
+
+    float currentAmplitude = startAmplitude;
+
+    while(currentAmplitude > endAmplitude)
+    {
+        currentAmplitude -= currentAmplitude * 0.084;
+        count++;
     }
 
-    float result = leftBrightness + (rightBrightness - leftBrightness) * parameter;
-
-    std::cout << "The brightness of the point is: " << result << std::endl;
+    std::cout << "The pendulum will swing " << count << " times before it stops." << std::endl;
 
     return 0;
 }
---
+++ практички/8.6 Практическая работа/task6.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <corecrt_math_defines.h>
+
+/*
+Задание 6. Маятник
+Что нужно сделать
+Известно, что амплитуда качающегося маятника с каждым разом затухает на 8,4% от
+амплитуды прошлого колебания. Если качнуть маятник, он, строго говоря, никогда
+не остановится: его амплитуда будет уменьшаться до тех пор, пока мы не сочтём
+такой маятник остановившимся. Напишите программу, определяющую, сколько раз
+качнётся маятник, прежде чем он, по нашему мнению, остановится. Программа
+получает на вход начальную амплитуду колебания в сантиметрах и конечную
+амплитуду его колебаний, которая считается остановкой маятника. Обеспечьте
+контроль ввода.
+*/
+
+int main()
+{
+    float startAmplitude, endAmplitude;
+    std::cout << "Enter start amplitude: ", std::cin >> startAmplitude;
+
+    while(startAmplitude <= 0)
+    {
+        std::cout << "Start amplitude must be greater than 0. Try again: ", std::cin >> startAmplitude;
+    }
+
+    std::cout << "Enter end amplitude: ", std::cin >> endAmplitude;
+
+    while(endAmplitude <= 0)
+    {
+        std::cout << "End amplitude must be greater than 0. Try again: ", std::cin >> endAmplitude;
+    }
+
+    if(startAmplitude < endAmplitude)
+    {
+        std::cout << "Start amplitude must be greater than end amplitude. Try again: ", std::cin >> startAmplitude, endAmplitude;
+    }
+    
+    int count = 0;
+
+    float currentAmplitude = startAmplitude;
+
+    while(currentAmplitude > endAmplitude)
+    {
+        currentAmplitude -= currentAmplitude * 0.084;
+        count++;
+    }
+
+    std::cout << "The pendulum will swing " << count << " times before it stops." << std::endl;
+
+    return 0;
+}
